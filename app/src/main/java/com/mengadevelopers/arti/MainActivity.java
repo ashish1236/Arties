e5019661ae90e044e62dff97c11681549498f7da
Ashish Gangwar <Ashish790673@gmail.com>
2022-11-06 17:57:25 +0930

    Initial commit

---
+++ app/src/main/java/com/mengadevelopers/arti/MainActivity.java
@@ -0,0 +1,263 @@
+package com.mengadevelopers.arti;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.gms.ads.AdError;
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.FullScreenContentCallback;
+import com.google.android.gms.ads.LoadAdError;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.android.gms.ads.interstitial.InterstitialAd;
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
+import com.mengadevelopers.arti.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+    private AdView mAdView;
+
+    private AdView mAdView4;
+    private InterstitialAd mInterstitialAd;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+
+        AdRequest adRequest = new AdRequest.Builder().build();
+
+
+
+        InterstitialAd.load(this, "ca-app-pub-9986980606552169/2821412642", adRequest,
+                new InterstitialAdLoadCallback() {
+                    @Override
+                    public void onAdLoaded(@NonNull InterstitialAd interstitialAd) {
+                        // The mInterstitialAd reference will be null until
+                        // an ad is loaded.
+                        mInterstitialAd = interstitialAd;
+                        mInterstitialAd.setFullScreenContentCallback(new FullScreenContentCallback() {
+                            @Override
+                            public void onAdClicked() {
+                                super.onAdClicked();
+                            }
+
+                            @Override
+                            public void onAdDismissedFullScreenContent() {
+                                // Called when ad is dismissed.
+                                // Set the ad reference to null so you don't show the ad a second time.
+
+                                mInterstitialAd = null;
+                            }
+
+                            @Override
+                            public void onAdFailedToShowFullScreenContent(AdError adError) {
+                                // Called when ad fails to show.
+
+                                mInterstitialAd = null;
+                            }
+
+                            @Override
+                            public void onAdImpression() {
+                                // Called when an impression is recorded for an ad.
+                                super.onAdImpression();
+                            }
+
+                            @Override
+                            public void onAdShowedFullScreenContent() {
+                                // Called when ad is shown.
+
+                            }
+                        });
+
+                    }
+
+                    @Override
+                    public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+                        // Handle the error
+
+                        mInterstitialAd = null;
+                    }
+                });
+
+
+
+        mAdView = findViewById(R.id.adView1);
+        mAdView.loadAd(adRequest);
+        mAdView.setAdListener(new AdListener() {
+            @Override
+            public void onAdClicked() {
+                super.onAdClicked();
+                // Code to be executed when the user clicks on an ad.
+            }
+
+            @Override
+            public void onAdClosed() {
+
+                // Code to be executed when the user is about to return
+                // to the app after tapping on an ad.
+            }
+
+            @Override
+            public void onAdFailedToLoad(LoadAdError adError) {
+                super.onAdFailedToLoad(adError);
+                mAdView.loadAd(adRequest);
+                // Code to be executed when an ad request fails.
+            }
+
+            @Override
+            public void onAdImpression() {
+                super.onAdImpression();
+                // Code to be executed when an impression is recorded
+                // for an ad.
+            }
+
+            @Override
+            public void onAdLoaded() {
+                super.onAdLoaded();
+                binding.lisasssasaasanearaaLayout6s12a.setVisibility(View.VISIBLE);
+                // Code to be executed when an ad finishes loading.
+            }
+
+            @Override
+            public void onAdOpened() {
+                super.onAdOpened();
+                // Code to be executed when an ad opens an overlay that
+                // covers the screen.
+            }
+        });
+
+
+
+
+        mAdView4 = findViewById(R.id.adView4);
+        mAdView4.loadAd(adRequest);
+        mAdView4.setAdListener(new AdListener() {
+            @Override
+            public void onAdClicked() {
+                super.onAdClicked();
+                // Code to be executed when the user clicks on an ad.
+            }
+
+            @Override
+            public void onAdClosed() {
+                // Code to be executed when the user is about to return
+                // to the app after tapping on an ad.
+            }
+
+            @Override
+            public void onAdFailedToLoad(LoadAdError adError) {
+                super.onAdFailedToLoad(adError);
+                mAdView4.loadAd(adRequest);
+                // Code to be executed when an ad request fails.
+            }
+
+            @Override
+            public void onAdImpression() {
+                super.onAdImpression();
+                // Code to be executed when an impression is recorded
+                // for an ad.
+            }
+
+            @Override
+            public void onAdLoaded() {
+                super.onAdLoaded();
+                binding.lisasssasaasanearaaLayout6s12d.setVisibility(View.VISIBLE);
+                // Code to be executed when an ad finishes loading.
+            }
+
+            @Override
+            public void onAdOpened() {
+                super.onAdOpened();
+                // Code to be executed when an ad opens an overlay that
+                // covers the screen.
+            }
+        });
+
+
+        binding.bill.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                adshow();
+                Intent intent = new Intent(MainActivity.this, webview.class);
+                intent.putExtra("key", "Ganesh");
+                startActivity(intent);
+            }
+        });
+        binding.elon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                adshow();
+                Intent intent = new Intent(MainActivity.this, webview.class);
+                intent.putExtra("key", "ambe");
+                startActivity(intent);
+            }
+        });
+        binding.JeffBezos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, webview.class);
+                intent.putExtra("key", "ShivChalisa");
+                startActivity(intent);
+            }
+        });
+
+        binding.LarryEllison.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                adshow();
+                Intent intent = new Intent(MainActivity.this, webview.class);
+                intent.putExtra("key", "Jagadish");
+                startActivity(intent);
+            }
+        });
+        binding.mark.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                adshow();
+                Intent intent = new Intent(MainActivity.this, webview.class);
+                intent.putExtra("key", "hanuman");
+                startActivity(intent);
+            }
+        });
+
+        binding.WarrenBuffet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                adshow();
+                Intent intent = new Intent(MainActivity.this, webview.class);
+                intent.putExtra("key", "santoshi");
+                startActivity(intent);
+            }
+        });
+        binding.BernardArnault.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                adshow();
+                Intent intent = new Intent(MainActivity.this, webview.class);
+                intent.putExtra("key", "laxmi");
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void adshow() {
+        if (mInterstitialAd != null) {
+            mInterstitialAd.show(MainActivity.this);
+        } else {
+
+        }
+    }
+    }
---
+++ app/src/main/java/com/mengadevelopers/arti/webview.java
@@ -0,0 +1,92 @@
+package com.mengadevelopers.arti;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.LoadAdError;
+import com.mengadevelopers.arti.databinding.ActivityWebviewBinding;
+
+public class webview extends AppCompatActivity {
+    Intent intent;
+    ActivityWebviewBinding binding;
+
+    private AdView mAdView2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding= ActivityWebviewBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        intent=getIntent();
+
+
+
+
+        mAdView2 = findViewById(R.id.adView2);
+        AdRequest adRequest2 = new AdRequest.Builder().build();
+        mAdView2.loadAd(adRequest2);
+        mAdView2.setAdListener(new AdListener() {
+            @Override
+            public void onAdClicked() {
+                super.onAdClicked();
+                // Code to be executed when the user clicks on an ad.
+            }
+
+            @Override
+            public void onAdClosed() {
+                // Code to be executed when the user is about to return
+                // to the app after tapping on an ad.
+            }
+
+            @Override
+            public void onAdFailedToLoad(LoadAdError adError) {
+                super.onAdFailedToLoad(adError);
+                mAdView2.loadAd(adRequest2);
+                // Code to be executed when an ad request fails.
+            }
+
+            @Override
+            public void onAdImpression() {
+                super.onAdImpression();
+                // Code to be executed when an impression is recorded
+                // for an ad.
+            }
+
+            @Override
+            public void onAdLoaded() {
+                super.onAdLoaded();
+
+                // Code to be executed when an ad finishes loading.
+            }
+
+            @Override
+            public void onAdOpened() {
+                super.onAdOpened();
+                // Code to be executed when an ad opens an overlay that
+                // covers the screen.
+            }
+        });
+
+
+
+
+
+
+
+        String key=intent.getStringExtra("key");
+        String file="file:///android_asset/";
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.getSettings().setAllowContentAccess(true);
+        myWebView.getSettings().setAllowFileAccess(true);
+        myWebView.loadUrl(file+key+".html");
+    }
+}
